ee6f24b15c1a21adc919677d3ea0b32e7fdf8cb7
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-07-31 18:21:52 +0200

    Introduce custom bucket schema with 11 buckets

--- src/engines/lynx_constants.hpp
+++ src/engines/lynx_constants.hpp
@@ -28,7 +28,18 @@ std::array<int, 6> phaseValues = {0, 1, 1, 2, 4, 0};
 
 constexpr static int EvalNormalizationCoefficient = 90;
 
-constexpr static int PSQTBucketCount = 8;
+constexpr static int PSQTBucketCount = 11;
+
+constexpr static std::array<int, 64> PSQTBucketLayout =
+    {
+        9, 9, 9, 9, 10, 10, 10, 10,
+        9, 9, 9, 9, 10, 10, 10, 10,
+        7, 7, 6, 6, 6, 6, 8, 8,
+        7, 7, 6, 6, 6, 6, 8, 8,
+        4, 4, 4, 6, 6, 5, 5, 5,
+        4, 4, 4, 1, 1, 5, 5, 5,
+        0, 0, 1, 1, 1, 3, 3, 3,
+        0, 0, 1, 2, 1, 3, 3, 3};
 
 constexpr static std::array<std::array<int, 12>, PSQTBucketCount> PieceValue = {
     {
