db14cd266e84f162db1a184b1fb5d426cbe78661
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-08-04 15:08:22 +0200

    Add custom schema 10 - individual buckets for first rank, and the rest in half-files

--- src/engines/lynx_base.hpp
+++ src/engines/lynx_base.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
-constexpr static int PSQTBucketCount = 16;
+constexpr static int PSQTBucketCount = 24;
+
+constexpr static std::array<int, 64> PSQTBucketLayout = {
+    16, 17, 18, 19, 20, 21, 22, 23,
+    16, 17, 18, 19, 20, 21, 22, 23,
+    16, 17, 18, 19, 20, 21, 22, 23,
+    16, 17, 18, 19, 20, 21, 22, 23,
+    8, 9, 10, 11, 12, 13, 14, 15,
+    8, 9, 10, 11, 12, 13, 14, 15,
+    8, 9, 10, 11, 12, 13, 14, 15,
+    0, 1, 2, 3, 4, 5, 6, 7, //
+};
